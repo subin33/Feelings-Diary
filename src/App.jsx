dcbc27f1d07197c25b166f5356e09ad9c3561351
subin33 <shtnqls33@gmail.com>
2025-02-18 21:27:41 +0900

    implement diary management feature 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,44 @@
 import './App.css'
-import { Routes, Route ,Link, useNavigate} from 'react-router-dom'
+import { useReducer } from 'react'
+import { Routes, Route} from 'react-router-dom'
 import Diary from './pages/Diary'
 import Home from './pages/Home'
 import New from './pages/New'
+import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
-import Button from "./components/Button"
-import Header from './components/Header'
 
-import { getEmotionImage } from './util/get-emotion-image'
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지 
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지 
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지 
-function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
+const mockData = [
+  {
+    id: 1,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
   }
+]
+function reducer(state,action){
+  return state;
+}
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
-  <Header 
-    title={"Header"} 
-    leftChild={<Button text={"Left"}/>}
-    rightChild={<Button text={"Right"}/>}
-  />
-  <Button text={"123"} onClick={() => {
-    console.log("123번 버튼 클릭!")
-  }}/>
-  <Button text={"123"} type={"POSITIVE"} onClick={() => {
-    console.log("123번 버튼 클릭!")
-  }}/>
-  <Button text={"123"} type={"NEGATIVE"} onClick={() => {
-    console.log("123번 버튼 클릭!")
-  }}/>
-
-  <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/new' element={<New />} />
-      <Route path='/diary/:id' element={<Diary />} />
-      <Route path='*' element={<Notfound />} />
-    </Routes>
-  </>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary/:id' element={<Diary />} />
+        <Route path='/edit/:id' element={<Edit />}/>
+        <Route path='*' element={<Notfound />} />
+      </Routes>
+    </>
   )
 }
 
