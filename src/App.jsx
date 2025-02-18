19ba6c37b414bbe1803d5821d9c57a612aaf185a
subin33 <shtnqls33@gmail.com>
2025-02-18 13:22:30 +0900

    page navigation, dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route ,Link, useNavigate} from 'react-router-dom'
 import Diary from './pages/Diary'
 import Home from './pages/Home'
 import New from './pages/New'
@@ -9,12 +9,30 @@ import Notfound from './pages/Notfound'
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지 
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지 
 function App() {
-  return <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/new' element={<New />} />
-    <Route path='/diary' element={<Diary />} />
-    <Route path='*' element={<Notfound />} />
-  </Routes>
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  }
+
+  return (
+  <>
+  <div>
+    <Link to={"/"}>Home</Link>
+    <Link to={"/new"}>New</Link>
+    <Link to={"/diary"}>Diary</Link>
+  </div>
+  <button onClick={onClickButton}>
+    New 페이지로 이동
+  </button>
+  <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/new' element={<New />} />
+      <Route path='/diary/:id' element={<Diary />} />
+      <Route path='*' element={<Notfound />} />
+    </Routes>
+  </>
+  )
 }
 
 export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,10 @@
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+  const [params, setParams] = useSearchParams();
+
+  console.log('params',params.get("value"))
+  console.log('setParams',setParams)
   return <div>Home</div>
 };
 
