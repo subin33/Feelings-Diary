b5e7ebb69241e7410caf040719342e0ad3610d68
subin33 <shtnqls33@gmail.com>
2025-02-19 10:00:48 +0900

    implement diary management feature 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useReducer, useRef } from 'react'
+import { createContext, useReducer, useRef } from 'react'
 import { Routes, Route} from 'react-router-dom'
 import Diary from './pages/Diary'
 import Home from './pages/Home'
@@ -28,8 +28,13 @@ function reducer(state, action){
     case 'UPDATE': return state.map((item) => 
       String(item.id) === String(action.data.id) ? action.data : item
     )
+    case "DELETE": return state.filter((item) => String(item.id) !== String(action.id))
+    default: return state
   }
 }
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
@@ -65,26 +70,45 @@ function App() {
 
 
   // 기존 일기 삭제
-
+  const onDelete = (id) => {
+    dispatch ({
+      type:"DELETE",
+      id
+    })
+  }
 
   return (
     <>
     <button  onClick={()=> {
       onCreate(new Date().getTime(), 1, "Hello")
-    }}>일기 추가 테스트 </button>
+    }}>일기 추가 테스트 
+    </button>
 
     <button onClick={() => {
       onUpdate(1, new Date().getTime(), 3, "수정된 일기 입니다")
     }}>
       일기 수정 테스트
     </button>
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/diary/:id' element={<Diary />} />
-        <Route path='/edit/:id' element={<Edit />}/>
-        <Route path='*' element={<Notfound />} />
-      </Routes>
+
+    <button  onClick={()=> {
+      onDelete(1)
+    }}>일기 삭제 테스트 
+    </button>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{
+        onCreate,
+        onUpdate,
+        onDelete
+      }}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/diary/:id' element={<Diary />} />
+          <Route path='/edit/:id' element={<Edit />}/>
+          <Route path='*' element={<Notfound />} />
+        </Routes>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
     </>
   )
 }
