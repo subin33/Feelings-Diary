fa7da02a03b20cac9c1ba46b137cc3197c25508e
subin33 <shtnqls33@gmail.com>
2025-02-27 12:16:01 +0900

    Implementing the Home Page: 2. Features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,44 @@
+import { useState,useContext } from "react";
+import { DiaryStateContext } from "../App";
+
 import Header from "../components/Header"
 import Button from "../components/Button"
 import DiaryList from "../components/DiaryList";
 
+const getMonthlyData = (pivotDate, data) => {
+
+  const beginTime = new Date(pivotDate.getFullYear(), 
+  pivotDate.getMonth(), 1, 0, 0, 0).getTime();
+
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() +1,
+    0,
+    23,59,59
+  ).getTime()
+
+  return data.filter((item)=>beginTime <= item.createdDate && item.createdDate <= endTime)
+}
+
 const Home = () => {
+  const data = useContext(DiaryStateContext);
+
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const monthlyData = getMonthlyData(pivotDate,data);
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() +1))
+  }
+  const inDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() -1))
+  }
   return (
   <div>
-    <Header title={"2024년 2월"}
-  leftChild={<Button text={"<"}/>}
-  rightChild={<Button text={">"} />}/>
-  <DiaryList />
+    <Header title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+  leftChild={<Button onClick={inDecreaseMonth} text={"<"}/>}
+  rightChild={<Button onClick={onIncreaseMonth} text={">"} />}/>
+  <DiaryList data={monthlyData} />
   </div>
 )
 };
