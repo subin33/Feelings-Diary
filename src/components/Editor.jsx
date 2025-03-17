0b2e26d8cc95763ec62888505b73799bee8023f3
subin33 <shtnqls33@gmail.com>
2025-03-17 14:21:18 +0900

    implementing new page 2. functionality

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,8 @@
 import "./Editor.css"
 import EmotionItem from "./EmotionItem"
 import Button from "./Button"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const emotionList = [
   {
@@ -25,30 +27,78 @@ const emotionList = [
   }
 ]
 
-const Editor = () => {
+const getStringedDate = (targetDate) => {
+  //날짜 YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() +1;
+  let date = targetDate.getDate();
 
-  const emotionId = 1;
+  if(month <10 ){
+    month = `0${month}`;
+  }
+  if(date < 10 ){
+    date = `0${date}`;
+  }
+  return `${year}-${month}-${date}`
+}
+
+const Editor = ({onSubmit}) => {
+  const nav = useNavigate();
+  const [input,setInput] = useState({
+    createdDate: new Date(),
+    emotionId : 3,
+    content:"",
+  });
+  const onChangeInput = (e) => {
+    let name = e.target.name; //어떤 요소에 입력이 들어온건지 
+    let value = e.target.value; //입력된 값이 무엇인지
+
+    if(name === 'createdDate'){
+      value = new Date(value);
+    }
+    setInput({
+      ...input,
+      [name]:value,
+
+    })
+  }
+
+  const onClickSubmitButton = () => {
+    onSubmit(input)
+  }
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)} 
+          type="date" 
+        />
       </section>
       <section className="emotion_section">
-        <h4>오늘의 기분</h4>
+        <h4>오늘의 감정</h4>
         <div className="emotion_list_wrapper">
          {emotionList.map((item)=> 
-         <EmotionItem key={item.emotionId} {...item} isSelected = {item.emotionId === emotionId}/>)}
+         <EmotionItem 
+         onClick={()=>onChangeInput({
+          target:{
+            name: "emotionId",
+            value: item.emotionId,
+          }
+         })}
+         key={item.emotionId} {...item} isSelected = {item.emotionId === input.emotionId}/>)}
         </div>
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?"/>
+        <textarea name="content" value={input.content} onChange={onChangeInput} placeholder="오늘은 어땠나요?"/>
       </section>
       <section className="button_section">
-        <Button text={"취소하기"}/>
-        <Button text={"작성완료"} type={"POSITIVE"}/>
+        <Button onClick={() => nav(-1)} text={"취소하기"}/>
+        <Button onClick={onClickSubmitButton} text={"작성완료"} type={"POSITIVE"}/>
       </section>
     </div>
   )
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,14 +1,32 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { useContext, } from "react";
+import { DiaryDispatchContext } from "../App";
+
 const New = () => {
+  const {onCreate} = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(
+      input.createdDate.getTime(),
+      input.emotionId,
+      input.content
+    );
+
+    // 작성이 완료되면, home 페이지로 이동하고, 이둥 후에는 replace:true 로 뒤로가기를 방지 
+    nav("/",{replace:true})
+  }
   return (
   <div>
     <Header 
       title={"새 일기 쓰기"}
-      leftChild={<Button text={"< 뒤로가기"} />}
+      leftChild={<Button onClick={() => nav(-1)}
+         text={"< 뒤로가기"} />}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
   </div>
 )
 };
