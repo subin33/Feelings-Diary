308c6ca3fa2dc6ce990f64b2191efec0a2b86e13
subin33 <shtnqls33@gmail.com>
2025-03-18 10:38:21 +0900

    using web storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { createContext, useReducer, useRef } from 'react'
+import { createContext, useEffect, useReducer, useRef, useState } from 'react'
 import { Routes, Route} from 'react-router-dom'
 import Diary from './pages/Diary'
 import Home from './pages/Home'
@@ -8,44 +8,76 @@ import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
 
 
-const mockData = [
-  {
-    id: 1,
-    createdDate: new Date("2025-03-26").getTime(),
-    emotionId: 1,
-    content: "1번 일기 내용",
-  },
-  {
-    id: 2,
-    createdDate: new Date("2025-03-25").getTime(),
-    emotionId: 2,
-    content: "2번 일기 내용",
-  },
-  {
-    id: 3,
-    createdDate: new Date("2025-02-01").getTime(),
-    emotionId: 3,
-    content: "3번 일기 내용",
-  }
-]
+
 function reducer(state, action){
+  let nextState;
   switch(action.type){
-    case 'CREATE': return [action.data, ...state];
-    case 'UPDATE': return state.map((item) => 
+    case 'INIT' : return action.data; 
+    case 'CREATE':{
+      nextState =[action.data, ...state];
+      break;
+    };
+    case 'UPDATE': {
+      nextState = state.map((item) => 
       String(item.id) === String(action.data.id) ? action.data : item
     )
-    case "DELETE": return state.filter((item) => String(item.id) !== String(action.id))
+    break;
+    }
+    case "DELETE":{
+      nextState = state.filter((item) => String(item.id) !== String(action.id)
+    )
+    break;
+    }
     default: return state
   }
+
+  localStorage.setItem("diary",JSON.stringify(nextState))
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
-function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3)
+// JSON.stringify -> 문자열로 변경 
+// JSON.parse -> 객체로 변경 
+// JSON.parse 를 사용할때, 값이 undefined나 null 이면 오류를 발생한다. 그래서 이점을 유의해서 사용해야한다. 
 
+// localStorage.setItem('person',JSON.stringify({name:"ddd"}))
+
+// console.log(JSON.parse(localStorage.getItem("person")))
+function App() {
+  // 로딩 상태를 보관하는 state 
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("diary");
+    if(!storedData){
+      setIsLoading(false);
+      return;
+    }
+    const parsedData = JSON.parse(storedData);
+    // Array.isArray -> 배열이라면 true 아니라면 false
+    if(!Array.isArray(parsedData)){
+      setIsLoading(false);
+      return;
+    }
+    let maxId = 0; 
+    parsedData.forEach((item) => {
+      if(Number(item.id) > maxId) {
+        maxId = Number(item.id)
+      }
+    });
+
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type:"INIT",
+      data:parsedData
+    })
+    setIsLoading(false);
+  },[])
   // 새로운 일기 추가 
   const onCreate = (createdDate, emotionId, content) => {
     dispatch({
@@ -83,6 +115,9 @@ function App() {
     })
   }
 
+  if(isLoading){
+    return <div>데이터 로딩중입니다...</div>
+  }
   return (
     <>
  
